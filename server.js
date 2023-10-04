8c2bd65331df9be7125d6bacb62c6a1826ad6c9e
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-10-04 11:35:57 +0530

    final
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
-
-const File = require('./models/File');
-const bcrypt = require('bcrypt');
-
 const multer = require('multer');
-const upload = multer({ dest: 'uploads' });
-
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL);
+const bcrypt = require('bcrypt');
+const File = require('./models/File');
 
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+
+const upload = multer({ dest: 'uploads' });
+
+mongoose.connect(process.env.DATABASE_URL);
 
 app.set('view engine', 'ejs');
 
@@ -20,38 +19,41 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload', upload.single('file'), async (req, res) => {
-	console.log('Upload Successful');
 	const fileData = {
 		path: req.file.path,
 		originalName: req.file.originalname,
 	};
-	if (req.body.password !== null && req.body.password !== '') {
+	if (req.body.password != null && req.body.password !== '') {
 		fileData.password = await bcrypt.hash(req.body.password, 10);
 	}
 
 	const file = await File.create(fileData);
+
 	res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
 app.route('/file/:id').get(handleDownload).post(handleDownload);
 
 async function handleDownload(req, res) {
-	if (file.password !== null) {
+	const file = await File.findById(req.params.id);
+
+	if (file.password != null) {
 		if (req.body.password == null) {
 			res.render('password');
 			return;
 		}
-	}
-	if (!(await bcrypt.compare(req.body.password, file.password))) {
-		res.render('password', { error: true });
-		return;
+
+		if (!(await bcrypt.compare(req.body.password, file.password))) {
+			res.render('password', { error: true });
+			return;
+		}
 	}
 
-	const file = await File.findById(req.params.id);
 	file.downloadCount++;
 	await file.save();
 	console.log(file.downloadCount);
+
 	res.download(file.path, file.originalName);
 }
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
