cdb9048a3afb36c71820e4623e00227751d47c12
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-10-04 11:31:55 +0530

    ppfs
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- models/File.js
+++ models/File.js
@@ -17,4 +17,4 @@ const File = new mongoose.Schema({
 	},
 });
 
-mongoose.model('File', File);
+module.exports = mongoose.model('File', File);
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+const File = require('./models/File');
+const bcrypt = require('bcrypt');
+
 const multer = require('multer');
 const upload = multer({ dest: 'uploads' });
 
@@ -8,6 +11,7 @@ mongoose.connect(process.env.DATABASE_URL);
 
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -15,8 +19,39 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-	res.send('Upload Successful');
+app.post('/upload', upload.single('file'), async (req, res) => {
+	console.log('Upload Successful');
+	const fileData = {
+		path: req.file.path,
+		originalName: req.file.originalname,
+	};
+	if (req.body.password !== null && req.body.password !== '') {
+		fileData.password = await bcrypt.hash(req.body.password, 10);
+	}
+
+	const file = await File.create(fileData);
+	res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
+
+app.route('/file/:id').get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
+	if (file.password !== null) {
+		if (req.body.password == null) {
+			res.render('password');
+			return;
+		}
+	}
+	if (!(await bcrypt.compare(req.body.password, file.password))) {
+		res.render('password', { error: true });
+		return;
+	}
+
+	const file = await File.findById(req.params.id);
+	file.downloadCount++;
+	await file.save();
+	console.log(file.downloadCount);
+	res.download(file.path, file.originalName);
+}
 
 app.listen(process.env.PORT || 3000);
